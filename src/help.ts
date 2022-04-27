a177be6810490f9f9094a4acd4e95643ed879672
Sam Davis <sdavis2@atlassian.com>
2022-04-27 13:23:37 +1000

    updates

--- src/help.ts
+++ src/help.ts
@@ -1,40 +1,10 @@
 import * as fs from "fs";
 import * as cliProgress from "cli-progress";
-
-// ===== EDIT VALUES BELOW =====
-
-const POSITION_EXCLUSIONS = [[], ["W"], [], [], []];
-const KNOWN_LETTER_POSITIONS_TEMPLATE: string = "S@O@N";
-const AVAILABLE_LETTERS = [
-    "Q",
-    "W",
-    // "E",
-    // "R",
-    // "T",
-    "Y",
-    "U",
-    "I",
-    "O",
-    "P",
-    // "A",
-    "S",
-    "D",
-    "F",
-    "G",
-    "H",
-    "J",
-    // "K",
-    "L",
-    "Z",
-    "X",
-    "C",
-    "V",
-    "B",
-    "N",
-    "M",
-];
-
-// ===== EDIT VALUES ABOVE =====
+import {
+    AVAILABLE_LETTERS,
+    KNOWN_LETTER_POSITIONS_TEMPLATE,
+    POSITION_EXCLUSIONS,
+} from "./vars";
 
 let KNOWN_LETTERS = [];
 POSITION_EXCLUSIONS.forEach((position) => {
